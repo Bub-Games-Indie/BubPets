4a49d25df64a0857df34e82570eaeae9ade28865
Bubeboi <43781076+Bubeboi@users.noreply.github.com>
2019-10-15 10:36:49 -0400

    Create BubPets.bat
    1.0 version of the game

---
+++ BubPets.bat
@@ -0,0 +1,26 @@
+@echo off
+:splash
+cls
+echo Welcome to Bub Pets Version 1.0!
+echo (:) -Hi
+echo Please press enter to begin
+set /p holder=
+goto chonamu
+
+:chonamu
+cls
+echo Please type in a nick name for your pet to call you.
+set /p uname=
+goto chonamp
+
+:chonamp
+cls
+echo Please pick a name for your Bub.
+set /p pname
+goto mm
+
+:mm
+cls
+echo WORK IN PROGRESS! PRESS ENTER
+set /p holder=
+exit
